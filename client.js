673f4ec161bdf317098f85490db58689110a47a1
Anxo Soto <mancontr@gmail.com>
2018-04-15 02:37:19 +0200

    Moved demo component to its own file, shared by each env.

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Root from './Root'
+
 ReactDOM.render(
-  <h1>Hola, mundo!</h1>,
+  <Root />,
   document.getElementById('root')
 )
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import express from 'express'
 
+import Root from './Root'
+
 const app = express()
 app.get('*', (req, res) => {
   res.send(ReactDOMServer.renderToString(
-    <h1>Hola, mundo!</h1>
+    <Root />
   ))
 })
 app.listen(3000)
