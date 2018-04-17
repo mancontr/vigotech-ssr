2457cdd1d944765c04b2c5f4bb9a186c5896f285
Anxo Soto <mancontr@gmail.com>
2018-04-17 21:09:27 +0200

    Added client-side fetch data support.

--- src/boilerplate/client.js
+++ src/boilerplate/client.js
@@ -2,16 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
+import { Router, browserHistory, applyRouterMiddleware } from 'react-router'
 
 import reducers from '../store/reducers'
 import routes from '../routes'
 
 const store = createStore(reducers, window.initialState)
 
+let isFirstRender = true
+const middleware = applyRouterMiddleware({
+  renderRouterContext: (child, props) => {
+    if (isFirstRender) {
+      // No fetch, as we did it already on server
+      isFirstRender = false
+      return child
+    }
+    props.routes.forEach((route) =>
+      route.component &&
+      route.component.fetchData &&
+      route.component.fetchData(props, store.dispatch, store.getState)
+    )
+    return child
+  }
+})
+
 ReactDOM.hydrate(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={browserHistory} render={middleware}>
       {routes}
     </Router>
   </Provider>,
